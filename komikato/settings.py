e8279de383c2d2ace41dd25d7f406401366a0ec7
JIMBE <adriyansyahmf0@gmail.com>
2025-04-17 21:42:22 +0700

    production

--- komikato/settings.py
+++ komikato/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'Asaedwe3w435rsfsdte465e6sdw43w5w335w353543534rfdsf43tqwrf344et54r6t@32q43242321'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
